fb33591992b8df8915107cdd3a19befc0ce55659
mcszen1 <mcszen@gmail.com>
2024-11-03 10:18:31 -0300

    Update graphgen3.py

--- graphgen3.py
+++ graphgen3.py
@@ -8,8 +8,9 @@ from io import BytesIO, StringIO
 st.image ("NIDLogo.jpg")
 st.title ("Gerador de Grafos Interativos Baseado em Ontologias")
 st.markdown ('*Ferramenta Desenvolvida pelo NID/LABCOM/UFMA  em projeto apoiado pela FAPEMA*')
-st.markdown ("_[Conheça o LABCOM](https://www.labcomdata.com.br/)_")
+st.markdown ("_[Base Conceitual](https://periodicos.ufsc.br/index.php/jornalismo/article/view/95037/55606)_")
 st.markdown (" **_[Para gerar uma ontologia para o seu texto use nossa ferramenta GRAPHMaker](https://chatgpt.com/g/g-HSkRbqVrk-graphmaker)_**")
+st.markdown ("Para citar: CARNEIRO DOS SANTOS, Marcio. Grafos de conhecimento: explorando o potencial de modelos de geração de linguagem. Estudos em Jornalismo e Mídia, v. 20, n. 2, 2023.
 
 
 st.write("Carregue um arquivo CSV com as colunas 'Node 1', 'Node 2' e 'Edge' para criar um gráfico de ontologia interativo.")
